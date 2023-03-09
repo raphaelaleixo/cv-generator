ac446c193ed1fec5fd67820a868bcf58c17d6fa6
Raphael Aleixo <raphael.aleixo@gmail.com>
2023-03-09 15:13:08 -0500

    changing data transformation code

--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,26 @@
 export const fetchFromSheets = async (page) => {
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const spreadSheet = process.env.REACT_APP_SPREADSHEET_KEY;
-    const response = await fetch(
-      `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheet}/values/${page}?alt=json&key=${apiKey}`
-    );
-    const data = await response.json();
-    const { values } = data;
-    const indexes = values.shift();
-    const returnArray = [];
-    values.forEach((value, index) => {
-      const returnObject = {};
-      value.forEach((column, index) => {
-        returnObject[indexes[index]] = column;
-      });
-      returnArray.push(returnObject);
-    });
-    return returnArray;
-  };
-  
-  export const sortExperiences = (a, b) => {
-    if (a.time > b.time) {
-      return -1;
-    }
-    if (a.time > b.time) {
-      return 1;
-    }
-    return 0;
-  };
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const spreadSheet = process.env.REACT_APP_SPREADSHEET_KEY;
+  const response = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheet}/values/${page}?alt=json&key=${apiKey}`
+  );
+  const data = await response.json();
+  const { values } = data;
+  const keys = values[0];
+  return values.slice(1).map((values) =>
+    values.reduce((obj, val, i) => {
+      obj[keys[i]] = val;
+      return obj;
+    }, {})
+  );
+};
+
+export const sortExperiences = (a, b) => {
+  if (a.time > b.time) {
+    return -1;
+  }
+  if (a.time > b.time) {
+    return 1;
+  }
+  return 0;
+};
